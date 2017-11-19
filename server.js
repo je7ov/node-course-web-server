200ebd092f1eb14e9cf92c3ff7c01d8ab9ba432b
Jeffrey Ovalles <jeffrey.ovalles@techclub.nyc>
2017-11-19 11:25:59 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+const express = require('express');
+
+const app = express();
+const port = 3001;
+
+app.set('view engine', 'pug');
+app.use((req, res, next) => {
+  // const now = new Date().toLocaleString();
+  const now = new Date().toString();  
+  const log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append to log file');
+    }
+  });
+  
+  next();
+});
+
+// app.use((req, res, next) => {
+//   res.render('maintenance');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+  res.render('home', {
+    currentYear: new Date().getFullYear()
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    pageTitle: 'About Page',
+    currentYear: new Date().getFullYear()
+  });
+});
+
+console.log("Starting server...");
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
+});
