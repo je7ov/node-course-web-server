1725b81c6c3668a5d2953ef0ac4049124b4a1dd8
Jeffrey Ovalles <jeffrey.ovalles@techclub.nyc>
2017-11-19 13:29:32 -0500

    Maintenance check

--- server.js
+++ server.js
@@ -19,9 +19,9 @@ app.use((req, res, next) => {
   next();
 });
 
-// app.use((req, res, next) => {
-//   res.render('maintenance');
-// });
+app.use((req, res, next) => {
+  res.render('maintenance');
+});
 
 app.use(express.static(__dirname + '/public'));
 
